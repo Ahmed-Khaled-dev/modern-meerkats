b6fc31902147dd6af0b5e4ada971cee46619c725
fliepeltje <donatas@rasiukevicius.dev>
2021-07-13 00:57:48 +0200

    Update MapWindow to use boxes to render content

--- app/windows/map.py
+++ app/windows/map.py
@@ -1,8 +1,7 @@
-from typing import Optional
-
 from pydantic import BaseModel
 
 from app import constants as const
+from app.types.hitbox import HitBox
 from app.windows.core import Window
 
 
@@ -11,11 +10,11 @@ class MapWindow(BaseModel):
 
     level_name: str
     level_number: int
+    boxes: list[HitBox]
     width: int = const.MAP_WIDTH
     height: int = const.MAP_HEIGHT
     pos_x: int = const.MAP_X
     pos_y: int = const.MAP_Y
-    content: Optional[str]
 
     def __str__(self) -> str:
         title = f"Level {self.level_number} - {self.level_name}"
@@ -25,6 +24,7 @@ class MapWindow(BaseModel):
             height=self.height,
             pos_x=self.pos_x,
             pos_y=self.pos_y,
-            content=self.content,
+            content=None,
         )
-        return str(window)
+        box_str = "".join([str(b) for b in self.boxes])
+        return str(window) + box_str
