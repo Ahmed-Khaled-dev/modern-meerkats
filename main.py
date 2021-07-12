4ed4a7f1df759970e87bb8fe9d9ead64cc60862f
fliepeltje <donatas@rasiukevicius.dev>
2021-07-13 01:00:56 +0200

    Update main to use test level

--- main.py
+++ main.py
@@ -1,36 +1,27 @@
-from blessed import Terminal
-
+from app.entities.player import Player
+from app.levels.level_test import TestLevel
 from app.screens.level import LevelScreen
 from app.types.commands import Command as Cmd
 from app.windows.cmd_help import CmdHelpWindow
 from app.windows.cmd_list import CmdListwindow
-from app.windows.map import MapWindow
 from app.windows.user_input import UserInputWindow
 
-sample_map = """
-###########           ##############
-#         #           #            #
-#     @   #           #     x      #
-#         #           #            #
-#         #           #            #
-###     ###           #####     ####
-  #     #                 #     #
-  #     #                 #     #
-  #     #                 #     #
-  #     ###################     #
-  #                             #
-  ###############################
-"""
-
-
-term = Terminal()
-
-ui = UserInputWindow()
-cmdh = CmdHelpWindow(allowed_commands=[Cmd.Move])
-level = LevelScreen(
-    map=MapWindow(level_name="Noob Garden", level_number=1, content=sample_map),
+level = TestLevel()
+player = Player(
+    start_y=7,
+    start_x=7,
+    moves=[],
+)
+player.add_move(10, "r")
+player.add_move(6, "d")
+player.add_move(10, "r")
+screen = LevelScreen(
+    map=level.initial(player),
     user_input=UserInputWindow(),
     cmd_help=CmdHelpWindow(allowed_commands=[Cmd.Move]),
     cmd_list=CmdListwindow(max_commands=4),
 )
-level.launch()
+screen.launch()
+seq = level.sequence(player)
+screen.animate_map(windows=seq)
+screen.launch()
