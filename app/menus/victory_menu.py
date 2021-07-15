0e64bd55efcf4f54936c26ce2d8c139b6abece92
fliepeltje <donatas@rasiukevicius.dev>
2021-07-15 21:24:29 +0200

    Update victory menu to be callable from level

--- app/menus/victory_menu.py
+++ app/menus/victory_menu.py
@@ -9,20 +9,18 @@ from asciimatics.renderers import FigletText, Rainbow, SpeechBubble
 from asciimatics.scene import Scene
 from asciimatics.screen import Screen
 
-screen = Screen.open()
 
-
-def update_screen(end_time: int, loop: object, screen: object) -> None:
+def update_screen(
+    end_time: int, loop: asyncio.AbstractEventLoop, screen: Screen
+) -> None:
     """Checks if the user input matches with keys s or q."""
     event = screen.get_event()
     screen.draw_next_frame()
 
     try:
-        event.key_code
-        if(event is not None and chr(event.key_code) == 'n'):
+        if event is not None and chr(event.key_code) == "n":
             loop.stop()
-
-        elif(event is not None and chr(event.key_code) == 'q'):
+        elif event is not None and chr(event.key_code) == "q":
             loop.stop()
         else:
             if loop.time() < end_time:
@@ -37,7 +35,7 @@ def update_screen(end_time: int, loop: object, screen: object) -> None:
             loop.stop()
 
 
-def victory(screen: object) -> None:
+def victory(screen: Screen) -> None:
     """Displays the victory screen."""
     scenes = []
     effects = [
@@ -45,17 +43,21 @@ def victory(screen: object) -> None:
         Print(
             screen,
             SpeechBubble("Press q to return to main menu."),
-            x=screen.width//2 - 40,
-            y=screen.height//2 + 3,
-            start_frame=5, transparent=True)
+            x=screen.width // 2 - 40,
+            y=screen.height // 2 + 3,
+            start_frame=5,
+            transparent=True,
+        ),
     ]
     effects.append(
         Print(
             screen,
             SpeechBubble("Press n to move to next level."),
-            x=screen.width//2 + 10,
-            y=screen.height//2 + 3,
-            start_frame=5, transparent=False)
+            x=screen.width // 2 + 10,
+            y=screen.height // 2 + 3,
+            start_frame=5,
+            transparent=False,
+        )
     )
 
     # display fireworks
@@ -77,12 +79,17 @@ def victory(screen: object) -> None:
                 randint(0, screen.width),
                 randint(screen.height // 8, screen.height * 3 // 4),
                 randint(start, stop),
-                start_frame=randint(0, 250)))
+                start_frame=randint(0, 250),
+            ),
+        )
 
     effects.append(
         Mirage(
             screen,
-            Rainbow(screen, FigletText("Victory", font="banner3")), screen.height // 2 - 17, colour=7)
+            Rainbow(screen, FigletText("Victory", font="banner3")),
+            screen.height // 2 - 17,
+            colour=7,
+        )
     )
     effects.append(
         Print(
@@ -91,19 +98,16 @@ def victory(screen: object) -> None:
             screen.height // 2 - 8,
             speed=1,
             start_frame=5,
-            transparent=False)
+            transparent=False,
+        )
     )
 
     scenes.append(Scene(effects, -1))
     screen.set_scenes(scenes)
-    loop = asyncio.get_event_loop()
+    loop = asyncio.new_event_loop()
     end_time = loop.time() + 500.0
     loop.call_soon(update_screen, end_time, loop, screen)
     loop.run_forever()
     loop.close()
     screen.clear()
     screen.close()
-
-
-if __name__ == "__main__":
-    victory(screen)
