3dd26728583cc4f40927ed145dfe8da4a43d854c
Ahmed <ahmed.khaled.yousry@gmail.com>
2021-07-15 23:08:10 +0200

    Added a new basic map design

--- app/levels/level_0.py
+++ app/levels/level_0.py
@@ -10,14 +10,27 @@ Level0 = Level(
     number=0,
     max_commands=7,
     description="yolo",
-    player=Player(start_x=7, start_y=7),
+    player=Player(start_x=7, start_y=3),
     entities=list(
         itertools.chain(
-            Wall.create_line(5, 5, 15, "h"),
-            Wall.create_line(5, 5, 5, "v"),
-            Wall.create_line(20, 5, 5, "v"),
-            Wall.create_line(5, 10, 7, "h"),
-            [Exit(pos_x=16, pos_y=7)],
+            # Start X Pos, Start Y Pos, Length, Vertical (v) or Horizontal (h)
+            # Left coloumn top horizontal line
+            Wall.create_line(1, 1, 15, "h"),
+            # Left vertical vertical line
+            Wall.create_line(1, 1, 38, "v"),
+            # Bottom big horizontal line
+            Wall.create_line(1, 38, 88, "h"),
+            # Right vertical line
+            Wall.create_line(88, 1, 38, "v"),
+            # Right coloumn top horizontal line
+            Wall.create_line(74, 1, 15, "h"),
+            # Middle right vertical line
+            Wall.create_line(73, 1, 28, "v"),
+            # Middle big horizontal line
+            Wall.create_line(16, 28, 57, "h"),
+            # Middle left vertical line
+            Wall.create_line(15, 1, 28, "v"),
+            [Exit(pos_x=80, pos_y=3)],
         )
     ),
     allowed_commands=[Cmd.Move],
